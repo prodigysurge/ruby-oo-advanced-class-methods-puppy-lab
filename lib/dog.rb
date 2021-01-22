382038cfb1e73e8812f77eea12a217f5b6717837
prodigysurge <boatinglife6@yahoo.com>
2021-01-22 08:41:44 -0500

    commit tree 18e00ab5c9b63667d4066aa7d91d0e93fab5dd32

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+    @@all = []
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        save
+        # @@print_all << name
+    end
+
+    def self.all
+        @@all
+    end
+    # binding.pry
+    def self.print_all
+        @@all.each do |j|
+            puts j.name
+        end
+    end
+
+    def self.clear_all
+        @@all.clear
+
+    end
+
+    def save
+        @@all << self
+    end
+
+end
+
+    
